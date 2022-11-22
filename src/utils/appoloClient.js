1e033b15d179d9b06da58db2e38d93d1b2fe736e
Thomas Woodfin <Twoodfin@berkeley.edu>
2022-11-22 15:18:32 -0700

    Update appoloClient.js

--- src/utils/appoloClient.js
+++ src/utils/appoloClient.js
@@ -1,9 +1,6 @@
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 import { API_URL } from '@env';
 
-// I have got and error for apollo client
-//undefined is not an object (evaluating 'obsQuery.reobserve.bind')
-// that's why I used the new client
 export const client = () => {
 	const cache = new InMemoryCache({
 		typePolicies: {
